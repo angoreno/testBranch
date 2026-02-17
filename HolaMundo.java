0764f5ac9ada4f26997af2dcce8bf4a453f5cb4b
Isaac Pizarro <52631387+angoreno@users.noreply.github.com>
2026-02-17 17:20:02 -0300

    add timezone and place

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,10 @@
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 public class HolaMundo {
     void main() {
         System.out.println("Hola Mundo");
+        System.out.println("Fecha y Hora: " + LocalDateTime.now());
+        System.out.println("Lugar: " + ZoneId.systemDefault());
     }
 }
